272ced231a049839e877ce9bcf493146ddf3623c
mascarabello <mascarabello@gmail.com>
2025-04-21 15:38:39 -0300

    Update 3_gastos_aquisicao_alimentacao_brasil.R

--- 02-consumo-de-alimentos/3_gastos_aquisicao_alimentacao_brasil.R
+++ 02-consumo-de-alimentos/3_gastos_aquisicao_alimentacao_brasil.R
@@ -11,25 +11,25 @@ p_load(dplyr, data.table, ggplot2, sf, googledrive, tidyr,RColorBrewer,readxl)
 path <- getwd()
 
 #Indica o caminho dos dados
-pathdir <- paste(path, "data/", sep = "/")
+intdir <-  paste(path, "data/intermediate/", sep = "/")
 outdir <-  paste(path, "data/outputs/", sep = "/")
 dicdir <-  paste(path, "data/dic_map/", sep = "/")
 
-source("consumo-de-alimentos/set_estrato.R")
+source("set_estrato.R")
 
 #A data de referência fi xada para a compilação, análise e apresentação dos resultados da POF 2017-2018 foi 15 de janeiro de 2018.
 #A data de referência fi xada para a compilação, análise e apresentação dos resultados da POF 2008-2009 foi 15 de janeiro de 2009.
 # Valor será corrigido de acordo com o dado do INPC : 1.69892970
 # Etapa 1: Leitura dos dados ----------------------------------------------
 #2008-2009
-base_aquisicao_alimentar_2008 <- read.csv(paste0(outdir,"tabela_base_alimentacao_pof0809_10marco2025.csv"),sep = ";")
-base_uc_2008 <- read.csv(paste0(outdir,"tabela_base_uc_pof0809_10marco2025.csv"),sep = ";")
+base_aquisicao_alimentar_2008 <- read.csv(paste0(intdir,"tabela_base_alimentacao_pof0809_10marco2025.csv"),sep = ";")
+base_uc_2008 <- read.csv(paste0(intdir,"tabela_base_uc_pof0809_10marco2025.csv"),sep = ";")
 
 base_uc_2008 <- base_uc_2008 %>% rename(UF = COD_UF) 
 
 #2017-2018
-base_aquisicao_alimentar_2018 <- read.csv(paste0(outdir,"tabela_base_alimentacao_pof1718_10marco2025.csv"),sep = ";")
-base_uc_2018 <- read.csv(paste0(outdir,"tabela_base_uc_pof1718_10marco2025.csv"),sep = ";")
+base_aquisicao_alimentar_2018 <- read.csv(paste0(intdir,"tabela_base_alimentacao_pof1718_10marco2025.csv"),sep = ";")
+base_uc_2018 <- read.csv(paste0(intdir,"tabela_base_uc_pof1718_10marco2025.csv"),sep = ";")
 
 #Tradutor - Aquisicao de alimentos
 tradutor_alimentacao_2008 <-
