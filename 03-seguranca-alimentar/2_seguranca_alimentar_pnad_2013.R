044b0719623d6b2553c78d4c4923de706ba08472
mascarabello <mascarabello@gmail.com>
2025-05-02 17:03:31 -0300

    Update 2_seguranca_alimentar_pnad_2013.R

--- 03-seguranca-alimentar/2_seguranca_alimentar_pnad_2013.R
+++ 03-seguranca-alimentar/2_seguranca_alimentar_pnad_2013.R
@@ -19,7 +19,8 @@ path <- getwd()
 #Indica o caminho dos dados
 pathdir <- paste(path, "data/raw/", sep = "/")
 dicdir <-  paste(path, "data/dic_map/", sep = "/")
-outdir <- paste(path, "data/outputs/", sep = "/")
+intdir <-  paste(path, "data/intermediate/", sep = "/")
+
 
 # Etapa 1: Leitura dos dados ----------------------------------------------------
 dicio_dom <-
@@ -33,7 +34,7 @@ col_specs <- fwf_positions(start = dicio_dom$posicao_inicial,
                            end = dicio_dom$end, 
                            col_names = dicio_dom$codigo)
 
-unzip(paste0(pathdir,"PNAD/2013/Dados_20170807.zip"),exdir = paste0(pathdir,"PNAD/2013/"))
+#unzip(paste0(pathdir,"PNAD/2013/Dados_20170807.zip"),exdir = paste0(pathdir,"PNAD/2013/"))
 pnad_2013 <- read_fwf(paste0(pathdir,"PNAD/2013/Dados/DOM2013.txt"), 
                       col_positions = col_specs, 
                       col_types = cols(.default = "c"))
@@ -109,89 +110,4 @@ pnad_2013_completo <- pnad_2013_completo %>%
 head(pnad_2013_completo)
 
 
-write.table(pnad_2013_completo, paste(outdir,"tabela_seguranca_alimentar_pnad_2013_26marco2025.csv", sep = ""),row.names = F, sep = ";")
-
-
-
-# Calculando totais para as porcentagens
-totais <- sumarizacao_segalimentar %>%
-  group_by(situacao_domicilio) %>%
-  summarise(
-    total_pessoas = sum(num_pessoas, na.rm = TRUE),
-    total_domicilios = sum(num_domicilios, na.rm = TRUE)
-  )
-
-# Unindo os totais com os dados principais
-dados_completos <- sumarizacao_segalimentar %>%
-  left_join(totais, by = "situacao_domicilio") %>%
-  mutate(
-    percentual_pessoas = round((num_pessoas / total_pessoas) * 100, 1),
-    percentual_domicilios = round((num_domicilios / total_domicilios) * 100, 1)
-  )
-
-# Reestruturando a tabela
-tabela_formatada <- dados_completos %>%
-  filter(seguranca_alimentar != "Não Aplicável") %>%
-  select(seguranca_alimentar, situacao_domicilio, num_pessoas, percentual_pessoas, num_domicilios, percentual_domicilios) %>%
-  pivot_wider(
-    names_from = situacao_domicilio,
-    values_from = c(num_pessoas, percentual_pessoas, num_domicilios, percentual_domicilios),
-    names_glue = "{situacao_domicilio}_{.value}"
-  )
-
-# Visualizando a tabela
-print(tabela_formatada)
-
-
-library(dplyr)
-library(tidyr)
-
-# Calculando totais para as porcentagens
-totais <- sumarizacao_segalimentar %>%
-  group_by(situacao_domicilio) %>%
-  summarise(
-    total_pessoas = sum(num_pessoas, na.rm = TRUE),
-    total_domicilios = sum(num_domicilios, na.rm = TRUE)
-  )
-
-# Unindo os totais com os dados principais
-dados_completos <- sumarizacao_segalimentar %>%
-  left_join(totais, by = "situacao_domicilio") %>%
-  mutate(
-    percentual_pessoas = round((num_pessoas / total_pessoas) * 100, 1),
-    percentual_domicilios = round((num_domicilios / total_domicilios) * 100, 1)
-  )
-
-# Criando a linha de Insegurança Alimentar Total
-inseguranca_total <- dados_completos %>%
-  filter(grepl("Insegurança Alimentar", seguranca_alimentar)) %>%
-  group_by(situacao_domicilio) %>%
-  summarise(
-    seguranca_alimentar = "Insegurança Alimentar",
-    num_pessoas = sum(num_pessoas),
-    num_domicilios = sum(num_domicilios),
-    total_pessoas = first(total_pessoas),
-    total_domicilios = first(total_domicilios)
-  ) %>%
-  mutate(
-    percentual_pessoas = round((num_pessoas / total_pessoas) * 100, 1),
-    percentual_domicilios = round((num_domicilios / total_domicilios) * 100, 1)
-  )
-
-# Combinando os dados com a linha de Insegurança Alimentar Total
-dados_completos_final <- bind_rows(dados_completos, inseguranca_total)
-
-# Reestruturando a tabela
-tabela_formatada <- dados_completos_final %>%
-  filter(seguranca_alimentar != "Não Aplicável") %>%
-  select(seguranca_alimentar, situacao_domicilio, num_pessoas, percentual_pessoas, num_domicilios, percentual_domicilios) %>%
-  pivot_wider(
-    names_from = situacao_domicilio,
-    values_from = c(num_pessoas, percentual_pessoas, num_domicilios, percentual_domicilios),
-    names_glue = "{situacao_domicilio}_{.value}"
-  ) %>%
-  arrange(factor(seguranca_alimentar, levels = c("Insegurança Alimentar Leve", "Insegurança Alimentar Moderada", "Insegurança Alimentar Grave", "Insegurança Alimentar", "Segurança Alimentar")))
-
-# Visualizando a tabela
-print(tabela_formatada)
-
+write.table(pnad_2013_completo, paste(intdir,"tabela_seguranca_alimentar_pnad_2013_26marco2025.csv", sep = ""),row.names = F, sep = ";")
